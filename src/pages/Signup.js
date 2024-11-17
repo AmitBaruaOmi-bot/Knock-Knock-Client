31f66b195f349a93b905bb2b816c0bd0185b0a11
AmitBaruaOmi <shunnoomi@gmail.com>
2024-11-17 19:19:26 +0000

    new

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
+import "./Signup.css"
+import { useNavigate } from'react-router-dom'
 export default function Signup() {
+
+  const navigate = useNavigate()
 
   const [userData, setUserData] = useState({ name: "", email: "", password: "", location: "" })
 
@@ -9,11 +13,16 @@ export default function Signup() {
     /*Synthetic event*/
     e.preventDefault();
    
-    axios.post("http://localhost:5005/auth/signup", { email: userData.email, password: userData.password, location: userData.location, name: userData.name })
-    .then((response)=>{
-      console.log(response)
+    axios.post(`http://localhost:5005/auth/signup`, { email: userData.email, password: userData.password, location: userData.location, name: userData.name })
+    .then((res) => {
+      console.log(userData.data)
+      res.json(userData.data)
+      navigate("/homepageafterlogin")
     })
-    
+    .catch((err) => {
+      console.log(err)
+      alert('User already exists')
+    })
   }
 
     const clickEvent = (event) => {
