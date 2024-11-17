f590f31f41eb048f3ed0877f58e4ff54dfd63d20
AmitBaruaOmi <shunnoomi@gmail.com>
2024-11-17 18:44:10 +0000

    new

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -15,7 +15,6 @@ export default function Homepage() {
 
       <div> <Navbar /> </div>
 
-<<<<<<< HEAD
       <div><Carousel /></div>
 
       <div className='m-3'>
@@ -28,14 +27,6 @@ export default function Homepage() {
        <Food/>
        <Food/>
        <Food/>
-=======
-      <div><Carousel/></div>
-      
-      <div className='m-3'> 
-        <Food/> 
-        <Food/> 
-        <Food/> 
->>>>>>> Commit
       </div>
 
       <div> <Footer /> </div>
